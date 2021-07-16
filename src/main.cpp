e4c7212739ef6c53dfd8b4cce566c9a74d33e247
ArneDJ <ArneDeJonghe@protonmail.com>
2021-07-16 19:58:08 +0200

    Project: add opengl and sdl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
 
 #include "extern/loguru/loguru.hpp"
+
+static const char* shaderCodeVertex = R"(
+#version 460 core
+layout (location=0) out vec3 color;
+const vec2 pos[3] = vec2[3](
+	vec2(-0.6, -0.4),
+	vec2( 0.6, -0.4),
+	vec2( 0.0,  0.6)
+);
+const vec3 col[3] = vec3[3](
+	vec3( 1.0, 0.0, 0.0 ),
+	vec3( 0.0, 1.0, 0.0 ),
+	vec3( 0.0, 0.0, 1.0 )
+);
+void main()
+{
+	gl_Position = vec4(pos[gl_VertexID], 0.0, 1.0);
+	color = col[gl_VertexID];
+}
+)";
+
+static const char* shaderCodeFragment = R"(
+#version 460 core
+layout (location=0) in vec3 color;
+layout (location=0) out vec4 out_FragColor;
+void main()
+{
+	out_FragColor = vec4(color, 1.0);
+};
+)";
 
 int main(int argc, char *argv[])
 {
@@ -8,9 +41,61 @@ int main(int argc, char *argv[])
 	loguru::init(argc, argv);
 	loguru::add_file("events.log", loguru::Append, loguru::Verbosity_MAX);
 
-	LOG_F(INFO, "Hello World!");
-	LOG_F(INFO, "I'm hungry for some %.3f!", 3.14159);
-	LOG_F(ERROR, "This is an error message");
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow("An SDL2 window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+
+	// Check that the window was successfully created
+	if (window == NULL) {
+		// In the case that the window could not be made...
+		LOG_F(ERROR, "Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {
+		return 1;
+	}
+
+	glClearColor(0.5f, 0.5f, 0.5f, 1.f);
+
+	const GLuint shaderVertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(shaderVertex, 1, &shaderCodeVertex, nullptr);
+	glCompileShader(shaderVertex);
+
+	const GLuint shaderFragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(shaderFragment, 1, &shaderCodeFragment, nullptr);
+	glCompileShader(shaderFragment);
+
+	const GLuint program = glCreateProgram();
+	glAttachShader(program, shaderVertex);
+	glAttachShader(program, shaderFragment);
+
+	glLinkProgram(program);
+	glUseProgram(program);
+
+	GLuint vao;
+	glCreateVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glViewport(0, 0, 640, 480);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	SDL_GL_SwapWindow(window);
+
+	SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+	SDL_GL_DeleteContext(glcontext);
+	// Close and destroy the window
+	SDL_DestroyWindow(window);
+
+	// Clean up
+	SDL_Quit();
 
 	return 0;
 }
