d745273dd9e95c81064597bb8037685efb9a7f96
ArneDJ <ArneDeJonghe@protonmail.com>
2021-11-02 17:56:26 +0100

    Physics: bumper block

--- src/physics/bumper.cpp
+++ src/physics/bumper.cpp
@@ -98,13 +98,26 @@ Bumper::Bumper(const glm::vec3 &origin, float radius, float length)
 void Bumper::update(btDynamicsWorld *world, float delta)
 {
 	if (glm::length(walk_direction) == 0.f) {
-		//return;
+		return;
 	}
+
+	// teleport body to desired location
+	// find collision hit points, hit normals and distance at location
+	// step back to location before first hit 
+	// add manifolds to collide and slide vector
+	// move body to final location
 
 	glm::vec3 velocity = delta * speed * walk_direction;
 
-	step_forward(world, velocity);
-	collide_and_slide(world, velocity, delta);
+	glm::vec3 altered_velocity = collide_and_slide(world, velocity);
+	for (int i = 0; i < 3; i++) {
+		altered_velocity = collide_and_slide(world, altered_velocity);
+	}
+	//collide_and_move(world, delta, speed * delta, velocity);
+	glm::vec3 displacement = collide_and_block(world, altered_velocity);
+	
+	glm::vec3 current_position = bt_to_vec3(ghost_object->getWorldTransform().getOrigin());
+	teleport(current_position + displacement);
 }
 	
 void Bumper::sync_transform()
@@ -123,7 +136,7 @@ void Bumper::teleport(const glm::vec3 &position)
 	ghost_object->setWorldTransform(t);
 }
 	
-void Bumper::step_forward(btDynamicsWorld *world, const glm::vec3 &velocity)
+glm::vec3 Bumper::collide_and_slide(btDynamicsWorld *world, const glm::vec3 &velocity)
 {
 	glm::vec3 current_position = bt_to_vec3(ghost_object->getWorldTransform().getOrigin());
 	glm::vec3 next_position = current_position + velocity;
@@ -153,16 +166,17 @@ void Bumper::step_forward(btDynamicsWorld *world, const glm::vec3 &velocity)
 		updated_velocity = desired_motion * glm::length(velocity);
 		//}
 		// Remove penetration (penetration epsilon added to handle infinitely small penetration):
-		float fraction = callback.m_closestHitFraction;
-		float depth = (1.f - fraction);
+		//float fraction = callback.m_closestHitFraction;
+		//float depth = (1.f - fraction);
 		//printf("%f\n", depth);
 		//current_position += (hit_normal * (depth + 0.001f));
 	}
 		
-	teleport(current_position + updated_velocity);
+	//teleport(current_position + updated_velocity);
+	return updated_velocity;
 }
 	
-void Bumper::collide_and_slide(btDynamicsWorld *world, const glm::vec3 &velocity, float delta)
+void Bumper::collide_and_move(btDynamicsWorld *world, float delta, float speed, const glm::vec3 &velocity)
 {
 	// TODO check what this does
 	//btVector3 minAabb, maxAabb;
@@ -172,13 +186,13 @@ void Bumper::collide_and_slide(btDynamicsWorld *world, const glm::vec3 &velocity
 	world->getDispatcher()->dispatchAllCollisionPairs(ghost_object->getOverlappingPairCache(), world->getDispatchInfo(), world->getDispatcher());
 
 	glm::vec3 current_position = bt_to_vec3(ghost_object->getWorldTransform().getOrigin());
-	glm::vec3 updated_velocity = glm::vec3(0.f);
+	glm::vec3 displacement = glm::vec3(0.f);
 
 	for (int i = 0; i < ghost_object->getOverlappingPairCache()->getNumOverlappingPairs(); i++) {
-		btBroadphasePair* pair = &ghost_object->getOverlappingPairCache()->getOverlappingPairArray()[i];
+		btBroadphasePair *pair = &ghost_object->getOverlappingPairCache()->getOverlappingPairArray()[i];
 
-		btCollisionObject* obj0 = static_cast<btCollisionObject*>(pair->m_pProxy0->m_clientObject);
-		btCollisionObject* obj1 = static_cast<btCollisionObject*>(pair->m_pProxy1->m_clientObject);
+		btCollisionObject *obj0 = static_cast<btCollisionObject*>(pair->m_pProxy0->m_clientObject);
+		btCollisionObject *obj1 = static_cast<btCollisionObject*>(pair->m_pProxy1->m_clientObject);
 
 		if ((obj0 && !obj0->hasContactResponse()) || (obj1 && !obj1->hasContactResponse())) {
 			continue;
@@ -190,25 +204,64 @@ void Bumper::collide_and_slide(btDynamicsWorld *world, const glm::vec3 &velocity
 		}
 		for (int j = 0; j < manifolds.size(); j++) {
 			btPersistentManifold* manifold = manifolds[j];
-			btScalar sign = manifold->getBody0() == ghost_object.get() ? btScalar(-1.0) : btScalar(1.0);
+			float sign = manifold->getBody0() == ghost_object.get() ? -1.f : 1.f;
 
 			for (int p = 0; p < manifold->getNumContacts(); p++) {
-				const btManifoldPoint& pt = manifold->getContactPoint(p);
+				const btManifoldPoint &point = manifold->getContactPoint(p);
 
-				btScalar dist = pt.getDistance();
+				// Remove penetration (penetration epsilon added to handle infinitely small penetration):
+				float dist = point.getDistance();
+				//printf("%f\n", dist);
 
-				btVector3 impulse = pt.m_normalWorldOnB * sign * dist;
-				updated_velocity += bt_to_vec3(impulse);
+				glm::vec3 impulse = bt_to_vec3(point.m_normalWorldOnB) * (sign * dist + 0.001f);
+				displacement += impulse;
 			}
 		}
 	}
 
-	//updated_velocity = glm::normalize(updated_velocity) * glm::length(velocity);
-	if (updated_velocity != glm::vec3(0.f)) {
-	//updated_velocity = glm::normalize(updated_velocity);
-	
-	teleport(current_position + updated_velocity);
+
+	if (displacement != glm::vec3(0.f)) {
+		current_position = glm::mix(current_position, current_position + displacement, 0.5f * glm::length(glm::normalize(displacement)));
+		teleport(current_position);
 	}
+}
+
+glm::vec3 Bumper::collide_and_block(btDynamicsWorld *world, const glm::vec3 &velocity)
+{
+	glm::vec3 displacement = velocity;
+
+	glm::vec3 current_position = bt_to_vec3(ghost_object->getWorldTransform().getOrigin());
+	glm::vec3 next_position = current_position + velocity;
+
+	glm::vec3 sweep_direction = current_position - next_position;
+
+	btTransform start = ghost_object->getWorldTransform();
+	btTransform end = start;
+	end.setOrigin(vec3_to_bt(next_position));
+
+	BumperConvexCallback callback(ghost_object.get(), vec3_to_bt(sweep_direction), btScalar(0.0));
+	callback.m_collisionFilterGroup = ghost_object->getBroadphaseHandle()->m_collisionFilterGroup;
+	callback.m_collisionFilterMask = ghost_object->getBroadphaseHandle()->m_collisionFilterMask;
+
+	ghost_object->convexSweepTest(shape.get(), start, end, callback, world->getDispatchInfo().m_allowedCcdPenetration);
+
+	glm::vec3 dir = glm::normalize(velocity);
+	float distance = glm::length(velocity);
+
+	if (callback.hasHit() && ghost_object->hasContactResponse()) {
+		glm::vec3 hit_normal = bt_to_vec3(callback.m_hitNormalWorld);
+		float fraction = callback.m_closestHitFraction;
+		float depth = fraction * glm::length(next_position - current_position);
+		//printf("distance %f\n", distance);
+		//printf("fraction %f\n", fraction);
+		//printf("depth %f\n", depth);
+		if (distance > depth) {
+			displacement = depth * dir;
+			//displacement = fraction
+		}
+	}
+
+	return displacement;
 }
 
 };
--- src/physics/bumper.h
+++ src/physics/bumper.h
@@ -5,8 +5,6 @@ public:
 	std::unique_ptr<geom::Transform> transform;
 	std::unique_ptr<btConvexShape> shape;
 	std::unique_ptr<btPairCachingGhostObject> ghost_object;
-	//std::unique_ptr<btRigidBody> body;
-	//std::unique_ptr<btMotionState> motion;
 	glm::vec3 walk_direction = {};
 	float speed = 10.f;
 	bool grounded = false;
@@ -19,8 +17,9 @@ public:
 	void update(btDynamicsWorld *world, float delta);
 	void teleport(const glm::vec3 &position);
 	void sync_transform();
-	void step_forward(btDynamicsWorld *world, const glm::vec3 &velocity);
-	void collide_and_slide(btDynamicsWorld *world, const glm::vec3 &velocity, float delta);
+	glm::vec3 collide_and_slide(btDynamicsWorld *world, const glm::vec3 &velocity);
+	void collide_and_move(btDynamicsWorld *world, float delta, float speed, const glm::vec3 &velocity);
+	glm::vec3 collide_and_block(btDynamicsWorld *world, const glm::vec3 &velocity);
 };
 
 };
